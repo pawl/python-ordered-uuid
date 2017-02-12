1db23edb33e8259f02bb60985bdf15254a5d87df
Paul Brown <paul90brown@gmail.com>
2017-02-12 02:19:26 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='python-ordered-uuid',
+    version='1.0.0',
+    description='A python implementation of Ordered UUID.',
+    url='https://github.com/pawl/python-ordered-uuid',
+    author='Paul Brown',
+    author_email='paul90brown+pypi@gmail.com',
+    py_modules=['ordered_uuid'],
+)
