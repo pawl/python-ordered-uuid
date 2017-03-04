081b1071644f495d9affd23f4be7b4cf7e2ccd52
Paul Brown <paul90brown@gmail.com>
2017-03-04 04:03:23 -0600

    bump version, add long_description

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except ImportError:
+    long_description = open('README.md').read()
+
 setup(
     name='python-ordered-uuid',
-    version='1.2.0',
+    version='1.3.1',
     description='A python implementation of Ordered UUID.',
     url='https://github.com/pawl/python-ordered-uuid',
     author='Paul Brown',
     author_email='paul90brown+pypi@gmail.com',
+    long_description=long_description,
     py_modules=['ordered_uuid'],
 )
